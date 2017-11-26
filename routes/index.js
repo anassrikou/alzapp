02f3b0cae0a48c36ef8b1fc8e408ef005535697c
revaxl <robin208@hotmail.fr>
2017-11-26 10:25:36 +0100

    fixed the templates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'login' });
 });
+
+router.get('/dashboard', function(req, res){
+  res.render('dashboard', {title: 'dashboard'})
+});
+
+router.get('/map', function(req, res){
+  res.render('map', {title: "maps"})
+});
+
 
 module.exports = router;
